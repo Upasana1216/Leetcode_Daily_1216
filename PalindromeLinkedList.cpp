285ad00b0cbf22473e415447ac8afe5de22ef437
Upasana1216 <222upasanasingh@gmail.com>
2024-03-22 15:22:24 +0530

    Create PalindromeLinkedList.cpp

---
+++ PalindromeLinkedList.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head, *fast =head;
+
+        while(fast && fast->next){
+            slow = slow->next; 
+            fast = fast->next->next;
+        }
+        if(fast != NULL && fast->next == NULL){
+            slow = slow->next;
+        }
+        ListNode* prev = nullptr, *nex;
+        while(slow != NULL && slow->next != NULL){
+            nex = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nex;
+        }
+
+        if(slow != NULL){
+            slow->next = prev;
+        }
+
+        fast = head;
+        while(slow && fast){
+            if(slow->val != fast->val){
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return true;
+    }
+};
